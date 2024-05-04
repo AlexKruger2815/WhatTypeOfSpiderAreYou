ded8f45c0168c06e8683b31750686ce4e06cb239
Ama <ama.commey@bbd.co.za>
2024-05-04 18:40:07 +0200

    Changed database connection to aws db

--- server/src/database_connection.js
+++ server/src/database_connection.js
@@ -2,10 +2,10 @@ const Pool = require ("pg").Pool;
 
 const pool = new Pool({
     user: "postgres",
-    host: "localhost",
-    database: "spider",
-    password: "postgres",
-    port: 5433,
+    host: "spider-type-rds-instance.cxzxxuebgdfx.eu-west-1.rds.amazonaws.com",
+    database: "SpiderTypeDB",
+    password: "GAqr2PEH-6Yx%4WHRgM{n5}}(9m|",
+    port: 5432,
 });
 
 module.exports = pool;
