cebc7bcd2cef8ecf9f3334fb6a8da2b26dee9016
Ama <ama.commey@bbd.co.za>
2024-05-05 18:53:26 +0200

    added cors to allow requests from any origin

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const endpoints = require("./api/routes");
+const cors = require('cors');
 const server = express();
 const port = 3000;
 
-
+server.use(cors())
 server.use(express.json());
 server.use("/api", endpoints);
 
