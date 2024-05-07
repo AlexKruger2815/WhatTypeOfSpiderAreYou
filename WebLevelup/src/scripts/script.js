a2af5f468bfe0502aa88a02c324ba30da69b024c
kdcgovender <kayleize.govender@bbd.co.za>
2024-05-07 07:27:43 +0200

    add results page and spider type logic

--- WebLevelup/src/scripts/script.js
+++ WebLevelup/src/scripts/script.js
@@ -6,12 +6,12 @@ const continueBtn = document.querySelector('.continue-btn');
 const quizSection = document.querySelector('.quiz-section');
 const quizBox = document.querySelector('.quiz-box');
 const resultBox = document.querySelector('.result-box');
-const tryAgainBtn = document.querySelector('.tryAgain-btn');
-const goHomeBtn = document.querySelector('.goHome-btn');
+const tryAgainBtn = document.querySelector('.retake-btn');
+const goHomeBtn = document.querySelector('.go-home-btn');
 const nextBtn = document.querySelector('.next-btn');
 const optionList = document.querySelector('.option-list');
 
-
+let spiderIds = [];
 
 startBtn.onclick = () => {
     popupInfo.classList.add('active');
@@ -36,7 +36,7 @@ let questionCount = 0;
 let questionNumber = 1;
 
 nextBtn.onclick = () => {
-    if (questionCount < questions.length) {
+    if (questionCount < questions.length-1) {
 
         questionCount++;
         showQuestions(questionCount);
@@ -47,17 +47,41 @@ nextBtn.onclick = () => {
         nextBtn.classList.remove('active');
     }
     else {
-
+        showResultBox();
     }
 }
 
+tryAgainBtn.onclick = () => {
+    quizBox.classList.add('active');
+    resultBox.classList.remove('active');
+    nextBtn.classList.remove('active');
 
+    questionCount = 0;
+    questionNumber = 1;
+    showQuestions(questionCount);
+    questionNumberCounter(questionNumber);
+}
+
+goHomeBtn.onclick = () => {
+    quizSection.classList.remove('active');
+    resultBox.classList.remove('active');
+    nextBtn.classList.remove('active');
+
+    questionCount = 0;
+    questionNumber = 1;
+    showQuestions(questionCount);
+    questionNumberCounter(questionNumber);
+}
 
 function showQuestions(index) {
 
     const questionText = document.querySelector('.question-text');
     questionText.textContent = `${index+1}. ${questions[index].question}`;
-    let optionTag = `<div class="option"><span>${questions[index].options[0]}</span></div> <div class="option"><span>${questions[index].options[1]}</span></div> <div class="option"><span>${questions[index].options[2]}</span></div> <div class="option"><span>${questions[index].options[3]}</span></div>`;
+    let optionTag = '';
+
+    questions[index].options.forEach(option => {
+        optionTag += `<div class="option" data-spider-id="${option.spiderID}"><span>${option.option}</span></div>`;
+    });
     optionList.innerHTML = optionTag;
 
     const option = document.querySelectorAll('.option');
@@ -69,17 +93,59 @@ function showQuestions(index) {
 function optionSelected(answer) {
     // Logic for spider type here
     // (maybe assign possible score related to spider)
+    
+    const allOptions = document.querySelectorAll('.option');
+    allOptions.forEach(option => {
+        option.classList.remove('selected');
+    });
     let userAnswer = answer.textContent;
-    let allOptions = optionList.children.length;
-
     answer.classList.add('selected')
-    for (let i = 0; i < allOptions; i++) {
-        optionList.children[i].classList.add('disabled');
-    }
-
     nextBtn.classList.add('active');
+
+    const spiderID = questions[questionCount].options.find(option => option.option === userAnswer).spiderID;
+
+    spiderIds.push(spiderID);
 }
+
 function questionNumberCounter(index) {
     const questionTotal = document.querySelector('.question-total');
     questionTotal.textContent = `${index} of ${questions.length} Questions`
+}
+
+function showResultBox() {
+    quizBox.classList.remove('active');
+    
+    const numbers = spiderIds.map(Number);
+
+    const spiderIdsSum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+    const averageSpider=Math.floor(spiderIdsSum/questions.length);
+    spiderIds=[];
+    fetch(`http://localhost:3000/api/spiders/${averageSpider}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(spiderData => {
+                const name = spiderData.Name;
+                const description = spiderData.Description;
+                const imageLink = spiderData.ImageLink;
+
+                const resultBox = document.querySelector('.result-box');
+                const spiderImage = resultBox.querySelector('.spider-image');
+                const resultText = resultBox.querySelector('.result-text');
+                const spiderDescription = resultBox.querySelector('.spider-description');
+
+                spiderImage.src = imageLink;
+                spiderImage.alt = name;
+                resultText.textContent = `Your Spider Type: ${name}`;
+                spiderDescription.textContent = description;
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+
+    resultBox.classList.add('active');
 }
