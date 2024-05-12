78a33e902b15938982e039e918ba9b6de0f715f2
Ama <ama.commey@bbd.co.za>
2024-05-12 14:59:41 +0200

    Brought back overwritten code.

--- WebLevelup/src/scripts/quiz-box.js
+++ WebLevelup/src/scripts/quiz-box.js
@@ -22,17 +22,28 @@ nextBtn.onclick = () => {
 function showQuestions(index) {
 
     const questionText = document.querySelector('.question-text');
-    questionText.textContent = `${index+1}. ${questions[index].question}`;
-    let optionTag = '';
+    questionText.textContent = `${index + 1}. ${questions[index].question}`;
+    const optionList = document.querySelector('.option-list');
+    Array.from(optionList.children).forEach(child => {
+        optionList.removeChild(child);
+    });
 
     questions[index].options.forEach(option => {
-        optionTag += `<div class="option" data-spider-id="${option.spiderID}"><span>${option.option}</span></div>`;
+        const optionDiv = document.createElement('section');
+        const optionSpan = document.createElement('span');
+
+        optionDiv.classList.add('option');
+        optionDiv.setAttribute('data-spider-id', option.spiderID);
+        optionSpan.textContent = option.option;
+        optionDiv.appendChild(optionSpan);
+        optionList.appendChild(optionDiv);
     });
-    optionList.innerHTML = optionTag;
 
     const option = document.querySelectorAll('.option');
     for (let i = 0; i < option.length; i++) {
-        option[i].setAttribute('onclick', 'optionSelected(this)');
+        option[i].addEventListener('click', function () {
+            optionSelected(this);
+        });
     }
     resetResultBox();
 }
@@ -74,8 +85,7 @@ function showResultBox() {
 
     spiderIds=[];
     
-    console.log('mode',modeSpider);
-    fetch(`http://localhost:3000/api/spiders/${modeSpider}`)
+    fetch(`/spiders/${modeSpider}`)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -103,61 +113,13 @@ function showResultBox() {
     results.style.visibility = 'visible';
 }
 
-function getRandomSource(sources) {
-    return sources[Math.floor(Math.random() * sources.length)];
-}
-
 function resetResultBox() {
     const spiderImage = document.querySelector('.spider-image');
-    const randomSource = getRandomSource(imageSources);
-    spiderImage.src = randomSource;
+    const source = "weaving-spider.gif";
+    spiderImage.src = source;
     spiderImage.alt = "spider gif";
     var resultText = document.querySelector('.result-text');
     resultText.textContent = "🕷️ Loading...";
     var spiderDescription = document.querySelector('.spider-description');
     spiderDescription.textContent = "";
-}
-
-function showResultBox() {
-    quizBox.style.display = 'none';
-    const numbers = spiderIds.map(Number);
-    const modeSpider = numbers.reduce((acc, curr) => {
-        acc[curr] = (acc[curr] || 0) + 1;
-        if (acc[curr] > acc.modeCount) {
-            acc.mode = curr;
-            acc.modeCount = acc[curr];
-        }
-        return acc;
-    }, { mode: null, modeCount: -1 }).mode;
-
-    spiderIds=[];
-    
-    fetch(`/spiders/${modeSpider}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(spiderData => {
-            const name = spiderData.Name;
-            const description = spiderData.Description;
-            const imageLink = spiderData.ImageLink;
-
-            const resultBox = document.querySelector('.result-box');
-            const spiderImage = resultBox.querySelector('.spider-image');
-            const resultText = resultBox.querySelector('.result-text');
-            const spiderDescription = resultBox.querySelector('.spider-description');
-
-            spiderImage.src = imageLink;
-            spiderImage.alt = name;
-            resultText.textContent = `Your Spider Type: ${name}`;
-            spiderDescription.textContent = description;
-        })
-        .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
-        });
-
-    resultBox.classList.add('active');
-
 }
